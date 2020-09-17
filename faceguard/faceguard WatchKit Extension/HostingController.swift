4d9b04666b778543e00cfa95a7efc5f1fa4a10c1
Karan Tamang <karandev@Karans-MBP.attlocal.net>
2020-09-17 17:16:25 -0400

    Created watch app

---
+++ faceguard/faceguard WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  faceguard WatchKit Extension
+//
+//  Created by KaranDev on 9/17/20.
+//  Copyright © 2020 KaranDev. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ faceguard/faceguard WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  faceguard WatchKit Extension
+//
+//  Created by KaranDev on 9/17/20.
+//  Copyright © 2020 KaranDev. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
