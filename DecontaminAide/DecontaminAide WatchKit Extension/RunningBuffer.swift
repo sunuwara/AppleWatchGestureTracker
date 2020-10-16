502dab582824d2411022bb2ad3c2909d0ab3bd17
sunuwara <sunuwara@mail.gvsu.edu>
2020-10-16 01:35:17 -0400

    Added running buffer implementation from Apple's sample program SwingWatch

---
+++ DecontaminAide/DecontaminAide WatchKit Extension/RunningBuffer.swift
@@ -0,0 +1,74 @@
+//
+//  RunningBuffer.swift
+//  DecontaminAide WatchKit Extension
+//
+//  Created by PGCapstone Team on 10/15/20.
+//  This class manages a running buffer of Double values.
+//  Implemented as provided in Apple's sample program SwingWatch
+//
+
+import Foundation
+
+class RunningBuffer {
+    // MARK: Properties
+    
+    var buffer = [Double]()
+    var size = 0
+
+    // MARK: Initialization
+    
+    init(size: Int) {
+        self.size = size
+        self.buffer = [Double](repeating: 0.0, count: self.size)
+    }
+    
+    // MARK: Running Buffer
+    
+    func addSample(_ sample: Double) {
+        buffer.insert(sample, at:0)
+        if buffer.count > size  {
+            buffer.removeLast()
+        }
+    }
+
+    func reset() {
+        buffer.removeAll(keepingCapacity: true)
+    }
+
+    func isFull() -> Bool {
+        return size == buffer.count
+    }
+
+    func sum() -> Double {
+        return buffer.reduce(0.0, +)
+    }
+
+    func min() -> Double {
+        var min = 0.0
+        if let bufMin = buffer.min() {
+            min = bufMin
+        }
+        return min
+    }
+
+    func max() -> Double {
+        var max = 0.0
+        if let bufMax = buffer.max() {
+            max = bufMax
+        }
+        return max
+    }
+
+    func recentMean() -> Double {
+        // Calculate the mean over the beginning half of the buffer.
+        let recentCount = self.size / 2
+        var mean = 0.0
+
+        if (buffer.count >= recentCount) {
+            let recentBuffer = buffer[0..<recentCount]
+            mean = recentBuffer.reduce(0.0, +) / Double(recentBuffer.count)
+        }
+
+        return mean
+    }
+}
