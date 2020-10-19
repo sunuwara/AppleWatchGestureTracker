df4c35f21c1225c8178a04813667353818426d03
sunuwara <sunuwara@mail.gvsu.edu>
2020-10-19 01:39:47 -0400

    Implement basic face touch detection (current not really accurate) similar to Apple Sample SwingWatch app

--- DecontaminAide/DecontaminAide WatchKit Extension/MotionManager.swift
+++ DecontaminAide/DecontaminAide WatchKit Extension/MotionManager.swift
@@ -21,39 +21,41 @@ class MotionManager: ObservableObject {
     
     // MARK: Application Specific Constants
     
-    // TODO: add threshold data
+    // Add threshold data
+    let rollThreshold = 0.80
+    let rateThreshold = 1.08
+    let resetThreshold = 1.08 * 0.05
     
     // The app is using 50hz data and the buffer is going to hold 1s worth of data.
-    let motionUpdateInterval = 1.0 / 50
+    let sampleInterval = 1.0 / 50
     let rateAlongGravityBuffer = RunningBuffer(size: 50)
-
+    
     var timer:Timer!
-    @Published var gravityStr = ""
-    @Published var accelerationStr = ""
-    @Published var rotationStr = ""
-    @Published var attitudeStr = ""
+    var recentDetection = false
     
     // Face touch count
-    var faceTouchCount = 0
-
-    var recentDetection = false
-
+    @Published var faceTouchCount = 0
 
     // MARK: Motion Manager
 
     func startUpdates() {
-        if(!motionManager.isDeviceMotionAvailable) {
+        if(motionManager.isDeviceMotionAvailable) {
+            // Reset everything when starting
+            resetAllState()
+            
+            motionManager.deviceMotionUpdateInterval = sampleInterval
+            motionManager.showsDeviceMovementDisplay = true
+            
+            // Retrieve motion updates
+            // TODO: implement Retrieving steady stream of motion updates in background
+            motionManager.startDeviceMotionUpdates()
+            
+            processDeviceMotion()
+        }
+        else {
             print("Device Motion is not available.")
             return
         }
-
-        // Reset everything when we start.
-        //resetAllState()
-
-        motionManager.deviceMotionUpdateInterval = motionUpdateInterval
-        motionManager.startDeviceMotionUpdates()
-        
-        processDeviceMotion()
     }
 
     func stopUpdates() {
@@ -65,112 +67,82 @@ class MotionManager: ObservableObject {
     // MARK: Motion Processing
     
     func processDeviceMotion() {
-        self.timer = Timer(fire:Date(), interval: (1.0 / 50.0), repeats: true, block: { (timer) in
+        
+        // Configure timer to fetch motion data
+        self.timer = Timer(fire:Date(), interval: sampleInterval, repeats: true, block: { [self] (timer) in
             if let data = self.motionManager.deviceMotion {
-                self.gravityStr = String(format: "X: %.1f Y: %.1f Z: %.1f" ,
-                                         data.gravity.x,
-                                         data.gravity.y,
-                                         data.gravity.z)
-                self.accelerationStr = String(format: "X: %.1f Y: %.1f Z: %.1f" ,
-                                              data.userAcceleration.x,
-                                              data.userAcceleration.y,
-                                              data.userAcceleration.z)
-                self.rotationStr = String(format: "X: %.1f Y: %.1f Z: %.1f" ,
-                                          data.rotationRate.x,
-                                          data.rotationRate.y,
-                                          data.rotationRate.z)
-                self.attitudeStr = String(format: "r: %.1f p: %.1f y: %.1f" ,
-                                          data.attitude.roll,
-                                          data.attitude.pitch,
-                                          data.attitude.yaw)
+                
+                // TODO: implement more accurate face touch detection
+                let gravity = data.gravity
+                let acceleration = data.userAcceleration
+                let rotationRate = data.rotationRate
+                let attitude = data.attitude
+                
+                let rateAlongGravity = rotationRate.x * gravity.x
+                                     + rotationRate.y * gravity.y
+                                     + rotationRate.z * gravity.z
+                self.rateAlongGravityBuffer.addSample(rateAlongGravity)
+
+                if (!self.rateAlongGravityBuffer.isFull()) {
+                    return
+                }
+
+                let accumulatedRollRot = self.rateAlongGravityBuffer.sum() * self.sampleInterval
+                let peakRate = accumulatedRollRot > 0 ? self.rateAlongGravityBuffer.max() : self.rateAlongGravityBuffer.min()
+
+                // Determine face touch
+                if(self.wristLocationIsLeft){
+                    if(accumulatedRollRot > rollThreshold && accumulatedRollRot < (2 * rollThreshold)) {
+                        if(peakRate > rateThreshold) {
+                            incrementFaceTouchCount()
+                        }
+                    }
+                }
                 
                 let timestamp = Date().timeIntervalSinceNow
-                os_log("Motion: %@, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@",
+                os_log("Motion: %@, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@",
                        String(timestamp),
-                       String(data.gravity.x),
-                       String(data.gravity.y),
-                       String(data.gravity.z),
-                       String(data.userAcceleration.x),
-                       String(data.userAcceleration.y),
-                       String(data.userAcceleration.z),
-                       String(data.rotationRate.x),
-                       String(data.rotationRate.y),
-                       String(data.rotationRate.z),
-                       String(data.attitude.roll),
-                       String(data.attitude.pitch),
-                       String(data.attitude.yaw))
+                       String(gravity.x),
+                       String(gravity.y),
+                       String(gravity.z),
+                       String(acceleration.x),
+                       String(acceleration.y),
+                       String(acceleration.z),
+                       String(rotationRate.x),
+                       String(rotationRate.y),
+                       String(rotationRate.z),
+                       String(attitude.pitch),
+                       String(attitude.roll),
+                       String(attitude.yaw),
+                       String(accumulatedRollRot),
+                       String(peakRate)
+                )
+
+                // Reset after letting the rate settle
+                if(self.recentDetection && abs(self.rateAlongGravityBuffer.recentMean()) < self.resetThreshold) {
+                    self.recentDetection = false
+                    self.rateAlongGravityBuffer.reset()
+                }
             }
         })
         
+        // Add timer to the current run loop
         RunLoop.current.add(self.timer!, forMode: .default)
-
-        /*
-        let gravity = deviceMotion.gravity
-        let rotationRate = deviceMotion.rotationRate
-
-        let rateAlongGravity = rotationRate.x * gravity.x // r⃗ · ĝ
-                             + rotationRate.y * gravity.y
-                             + rotationRate.z * gravity.z
-        rateAlongGravityBuffer.addSample(rateAlongGravity)
-
-        if !rateAlongGravityBuffer.isFull() {
-            return
-        }
-
-        let accumulatedYawRot = rateAlongGravityBuffer.sum() * sampleInterval
-        let peakRate = accumulatedYawRot > 0 ?
-            rateAlongGravityBuffer.max() : rateAlongGravityBuffer.min()
-
-        if (accumulatedYawRot < -yawThreshold && peakRate < -rateThreshold) {
-            // Counter clockwise swing.
-            if (wristLocationIsLeft) {
-                incrementBackhandCountAndUpdateDelegate()
-            } else {
-                incrementForehandCountAndUpdateDelegate()
-            }
-        } else if (accumulatedYawRot > yawThreshold && peakRate > rateThreshold) {
-            // Clockwise swing.
-            if (wristLocationIsLeft) {
-                incrementForehandCountAndUpdateDelegate()
-            } else {
-                incrementBackhandCountAndUpdateDelegate()
-            }
-         
-        }
-        */
-
-        // TODO: Reset after letting the rate settle to catch the return swing.
-        /*
-        if (recentDetection && abs(rateAlongGravityBuffer.recentMean()) < resetThreshold) {
-            recentDetection = false
-            rateAlongGravityBuffer.reset()
-        }
-        */
     }
 
-    // MARK: Data and Delegate Management
-    /*
+    // MARK: Data Management
+    
     func resetAllState() {
         rateAlongGravityBuffer.reset()
 
         faceTouchCount = 0
         recentDetection = false
-
-        updateFaceTouchDelegate()
     }
-
-    func incrementFaceTouchCountAndUpdateDelegate() {
+    
+    func incrementFaceTouchCount() {
         if(!recentDetection) {
             faceTouchCount += 1
             recentDetection = true
-
-            print("Face touch count: \(faceTouchCount)")
-            updateFaceTouchDelegate()
         }
     }
-
-    func updateFaceTouchDelegate() {
-        delegate?.didUpdateFaceTouchCount(self, faceTouchCount: faceTouchCount)
-    }
-     */
 }
