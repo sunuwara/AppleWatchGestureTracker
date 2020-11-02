f4f7726643cca1c9fb7a7a9c1b1bf585d60b6ece
sunuwara <sunuwara@mail.gvsu.edu>
2020-11-01 21:17:41 -0500

    Integrate motion manager into workout manager code, fixed face touch detection

--- DecontaminAide/DecontaminAide WatchKit Extension/WorkoutManager.swift
+++ DecontaminAide/DecontaminAide WatchKit Extension/WorkoutManager.swift
@@ -7,19 +7,45 @@
 
 import Foundation
 import HealthKit
-import Dispatch
+import CoreMotion
+import WatchKit
+import os.log
 
-class WorkoutManager: NSObject, ObservableObject, MotionManagerDelegate {
+class WorkoutManager: ObservableObject {
     
     // MARK: Properties
     
+    // HealthKit and Workout session
     let healthStore = HKHealthStore()
     var session: HKWorkoutSession!
-    let motionManager = MotionManager()
-
+    
+    // Motion management
+    let motionManager = CMMotionManager()
+    let wristLocationIsLeft = WKInterfaceDevice.current().wristLocation == .left
+    
+    // Sound management
+    //let soundManager = SoundManager()
+    
+    // MARK: Application Specific Constants
+    
+    // Add threshold data
+    let rollThreshold = 0.85
+    let rateThreshold = 1.35
+    let resetThreshold = 1.35 * 0.05
+    
+    // The app is using 50hz data and the buffer is going to hold 1s worth of data.
+    let sampleInterval = 1.0 / 50
+    let rateAlongGravityBuffer = RunningBuffer(size: 50)
+    let activity = 0
+    
+    var timer:Timer!
+    var recentDetection = false
+    var startTime = 0
+    
     @Published var faceTouchCount = 0
-
-    // workout configuration
+    
+    // MARK: Workout Manager
+    
     func workoutConfiguration() -> HKWorkoutConfiguration {
         let configuration = HKWorkoutConfiguration()
         configuration.activityType = .other
@@ -27,8 +53,6 @@ class WorkoutManager: NSObject, ObservableObject, MotionManagerDelegate {
         
         return configuration
     }
-    
-    // MARK: Workout Manager
     
     func startWorkout() {
         // If we have already started the workout, then do nothing.
@@ -45,7 +69,7 @@ class WorkoutManager: NSObject, ObservableObject, MotionManagerDelegate {
         
         // Start the workout session and begin device motion updates
         session.startActivity(with: Date())
-        motionManager.startUpdates()
+        startUpdates()
     }
     
     func stopWorkout() {
@@ -55,40 +79,123 @@ class WorkoutManager: NSObject, ObservableObject, MotionManagerDelegate {
         }
 
         // Stop the device motion updates and workout session.
-        motionManager.stopUpdates()
+        stopUpdates()
         session.end()
         
         // Clear the workout session.
         session = nil
     }
     
-    // MARK: MotionManagerDelegate
+    // MARK: Motion Manager
+
+    func startUpdates() {
+        if(motionManager.isDeviceMotionAvailable) {
+            // Reset everything when starting
+            resetAllState()
+            
+            motionManager.deviceMotionUpdateInterval = sampleInterval
+            motionManager.showsDeviceMovementDisplay = true
+            
+            // Retrieve motion updates
+            motionManager.startDeviceMotionUpdates()
+            
+            startTime = Int(NSDate().timeIntervalSince1970)
+            processDeviceMotion()
+        }
+        else {
+            print("Device Motion is not available.")
+            return
+        }
+    }
+
+    func stopUpdates() {
+        if(motionManager.isDeviceMotionAvailable) {
+            motionManager.stopDeviceMotionUpdates()
+        }
+    }
+
+    // MARK: Motion Processing
     
-    func didUpdateFaceTouchCount(_ manager: MotionManager, faceTouchCount: Int) {
-        DispatchQueue.main.async {
-            self.faceTouchCount = faceTouchCount
+    func processDeviceMotion() {
+        // Configure timer to fetch motion data
+        self.timer = Timer(fire:Date(), interval: sampleInterval, repeats: true, block: { [self] (timer) in
+            if let data = self.motionManager.deviceMotion {
+                
+                // TODO: implement more accurate face touch detection
+                let gravity = data.gravity
+                let acceleration = data.userAcceleration
+                let rotationRate = data.rotationRate
+                let attitude = data.attitude
+                
+                let rateAlongGravity = rotationRate.x * gravity.x
+                                     + rotationRate.y * gravity.y
+                                     + rotationRate.z * gravity.z
+                self.rateAlongGravityBuffer.addSample(rateAlongGravity)
+
+                if (!self.rateAlongGravityBuffer.isFull()) {
+                    return
+                }
+
+                let accumulatedRateAlongGravity = self.rateAlongGravityBuffer.sum() * self.sampleInterval
+                let peakRate = accumulatedRateAlongGravity > 0 ? self.rateAlongGravityBuffer.max() : self.rateAlongGravityBuffer.min()
+
+                // Determine face touch
+                if(accumulatedRateAlongGravity > rollThreshold && accumulatedRateAlongGravity < (2 * rollThreshold)) {
+                    if(peakRate > rateThreshold) {
+                        incrementFaceTouchCount()
+                    }
+                }
+                
+                let timeInterval = Int(NSDate().timeIntervalSince1970) - startTime
+                os_log("Motion: %@, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@",
+                       String(timeInterval),
+                       String(activity),
+                       String(acceleration.x),
+                       String(acceleration.y),
+                       String(acceleration.z),
+                       String(rotationRate.x),
+                       String(rotationRate.y),
+                       String(rotationRate.z),
+                       String(gravity.x),
+                       String(gravity.y),
+                       String(gravity.z),
+                       String(attitude.pitch),
+                       String(attitude.roll),
+                       String(attitude.yaw),
+                       String(accumulatedRateAlongGravity),
+                       String(peakRate)
+                )
+
+                // Reset after letting the rate settle
+                if(self.recentDetection && abs(self.rateAlongGravityBuffer.recentMean()) < self.resetThreshold) {
+                    self.recentDetection = false
+                    self.rateAlongGravityBuffer.reset()
+                    
+                    //TODO: commented out to fix face touch detection
+                    //soundManager.stopAudioEngine()
+                }
+            }
+        })
+        
+        // Add timer to the current run loop
+        RunLoop.current.add(self.timer!, forMode: .default)
+    }
+
+    // MARK: Data Management
+    
+    func resetAllState() {
+        rateAlongGravityBuffer.reset()
+
+        faceTouchCount = 0
+        recentDetection = false
+    }
+    
+    func incrementFaceTouchCount() {
+        if(!recentDetection) {
+            faceTouchCount += 1
+            recentDetection = true
+            
+            //soundManager.startAudioEngine()
         }
     }
 }
-
-// MARK: - HKWorkoutSessionDelegate
-//extension WorkoutManager: HKWorkoutSessionDelegate {
-//    func workoutSession(_ workoutSession: HKWorkoutSession, didChangeTo toState: HKWorkoutSessionState,
-//                        from fromState: HKWorkoutSessionState, date: Date) {
-//        // Wait for the session to transition states before ending the builder.
-//        /// - Tag: SaveWorkout
-//        if toState == .ended {
-//            print("The workout has now ended.")
-//            builder.endCollection(withEnd: Date()) { (success, error) in
-//                self.builder.finishWorkout { (workout, error) in
-//                    // Optionally display a workout summary to the user.
-//                    self.resetWorkout()
-//                }
-//            }
-//        }
-//    }
-//
-//    func workoutSession(_ workoutSession: HKWorkoutSession, didFailWithError error: Error) {
-//
-//    }
-//}
